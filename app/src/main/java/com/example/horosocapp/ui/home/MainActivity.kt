cb9263cb8ded44a8521b003beb6c5b1a99488bd3
Brandon <brandon.rapalino@grupodigital.co>
2023-10-21 00:08:46 -0500

    first commit

---
+++ app/src/main/java/com/example/horosocapp/ui/home/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.horosocapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horosocapp.R
+import com.example.horosocapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI(){
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
